2278d2819718c6bffe365614c4b291eafb0f30b7
SalehMehrabani <salehmehrabani.business@gmail.com>
2024-09-28 15:45:30 +0330

    categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,10 +1,10 @@
 import { motion } from "framer-motion";
 function Categories() {
   return (
-    <section className="w-full h-screen grid grid-cols-5 grid-rows-2 p-5 gap-5">
+    <section className="w-full min-h-screen h-max sm:h-screen grid sm:grid-cols-5 sm:grid-rows-2 p-5 gap-5 grid-row-3">
       {/* Category1 */}
       <div
-        className="col-start-1 col-end-4 row-start-1 row-end-2 flex flex-col justify-between items-start p-10 text-stone-800"
+        className="col-start-1 col-end-2 row-start-1 row-end-2 sm:col-start-1 sm:col-end-4 sm:row-start-1 sm:row-end-2 flex flex-col justify-between gap-10 items-start p-10 text-stone-800"
         style={{
           backgroundImage: "url(/images/Category1.jpg)",
           backgroundRepeat: "no-repeat",
@@ -16,7 +16,7 @@ function Categories() {
           initial={{ opacity: 0, y: -20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ ease: "easeOut", transition: 0.3, delay: 0.3 }}
-          className="w-1/2 uppercase text-3xl  tracking-tight"
+          className="w-full md:w-1/2 uppercase text-3xl  tracking-tight"
         >
           What to wear for this week ahead
         </motion.span>
@@ -31,7 +31,7 @@ function Categories() {
       </div>
       {/* Category2*/}
       <div
-        className="col-start-4 col-end-6 row-start-1 row-end-2 shadow"
+        className="hidden sm:grid sm:col-start-4 sm:col-end-6 sm:row-start-1 sm:row-end-2 shadow"
         style={{
           backgroundImage: "url(/images/Category2.jpg)",
           backgroundRepeat: "no-repeat",
@@ -41,7 +41,7 @@ function Categories() {
       ></div>
       {/* Category3*/}
       <div
-        className="col-start-1 col-end-3 row-start-2 row-end-3 flex flex-col justify-between items-start p-10 text-stone-800"
+        className="col-start-1 col-end-2 row-start-2 row-end-3 sm:col-start-1 sm:col-end-3 sm:row-start-2 sm:row-end-3 flex flex-col justify-between gap-10 items-start p-10 text-stone-800"
         style={{
           backgroundImage: "url(/images/Category3.jpg)",
           backgroundRepeat: "no-repeat",
@@ -53,7 +53,7 @@ function Categories() {
           initial={{ opacity: 0, y: -20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ ease: "easeOut", transition: 0.3, delay: 0.3 }}
-          className="w-1/2 uppercase text-3xl  tracking-tight"
+          className="w-full md:w-1/2 uppercase text-3xl  tracking-tight"
         >
           Endless Summer Day
         </motion.span>
@@ -68,7 +68,7 @@ function Categories() {
       </div>
       {/* Category4*/}
       <div
-        className="col-start-3 col-end-6 row-start-2 row-end-3 flex flex-col justify-between items-start p-10 text-stone-800"
+        className="col-start-1 col-end-2 row-start-3 row-end-4 sm:col-start-3 sm:col-end-6 sm:row-start-2 sm:row-end-3 flex flex-col justify-between items-start gap-10 p-10 text-stone-800"
         style={{
           backgroundImage: "url(/images/Category4.jpg)",
           backgroundRepeat: "no-repeat",
@@ -80,7 +80,7 @@ function Categories() {
           initial={{ opacity: 0, y: -20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ ease: "easeOut", transition: 0.3, delay: 0.3 }}
-          className="w-1/2 uppercase text-3xl  tracking-tight"
+          className="w-full md:w-1/2 uppercase text-3xl  tracking-tight"
         >
           The undisputed warm weather hero
         </motion.span>
